e2dfe659b5043bf00e6ccad6b9f5aac5250cb5dd
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-14 11:59:18 -0500

    Added get project by ID endpoint with a validateProjectID middlewear, postman tested

---
+++ middlewear/validateProjectId.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const project_db = require('../data/helpers/projectModel');
+
+function validateProjectId(req, res, next){
+    project_db.get(req.params.id)
+    .then(project => {
+        if(req.params.id != project.id){
+            res.status(400).json({message: "Invalid Project ID. "});
+        }else{
+            req.project = project
+            next();
+        }
+    })
+    .catch(error => {
+        res.status(500).json({error: "There was trouble retrieving project."});
+    })
+}
+
+module.exports = validateProjectId;
--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -1,12 +1,44 @@
 const express = require('express');
 const project_db = require('../data/helpers/projectModel');
+const validateProjectId = require('../middlewear/validateProjectId');
 
 
 const router = express.Router();
 
-
+//GET METHODS
 router.get('/', (req, res) => {
-    res.status(200).json('/api/projects/ endpoint working');
+    project_db.get()
+    .then(project => {
+        if(project.length === 0){
+            res.status(400).json({message: "There are currently no projects. Please add a project."});
+        }else{
+            res.status(200).json(project);
+        }
+    })
+    .catch(error => {
+        res.status(500).json({error: "There was a problem retrieving the prohects"});
+    });
+});
+
+router.get('/:id', validateProjectId, (req, res) => {
+    const { id } = req.project;
+    
+    project_db.get(id)
+    .then(project => {
+        if(project.length === 0){
+            res.status(400).json({message: "There are currently no projects. Please add a project."});
+        }else{
+            res.status(200).json(project);
+        }
+    })
+    .catch(error => {
+        res.status(500).json({error: "There was a problem retrieving the prohects"});
+    });
 })
 
+//POST METHODS
+
+//PUT/UPDATE METHODS
+
+//DELETE METHODS
 module.exports = router;
